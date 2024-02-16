987f89d4e3fbfaa57187a8d3a3d849535f6d6e41
manjusomu <manjusomu2001@gmail.com>
2024-02-16 10:22:18 +0530

    changes

---
+++ Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Calc
+    {
+        public void Add(double n1,double n2)
+        {
+            Console.WriteLine("Result after addition :\t"+(n1+n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after Substraction :\t" + (n1 - n2));
+        }
+        public void Multi(double n1, double n2)
+        {
+            Console.WriteLine("Result after multiplication :\t" + (n1 * n2));
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after division :\t" + (n1 / n2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+Calc cal = new Calc();
+cal.Add(8, 45);
+cal.Sub(45, 8);
+cal.Multi(7, 8);
+cal.Div(20, 5);
+
+
